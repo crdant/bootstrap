fe7bd21a587617a364e26f1fc576fc266384ad55
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-07 19:00:24 -0400

    Quickly set up an environment for testing script fragments

---
+++ env.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+BASEDIR=`dirname $0`
+. "${BASEDIR}/lib/env.sh"
+
+export ACCOUNT
+export DOMAIN
+export PROJECT
+
+export DOMAIN_TOKEN
+export SUBDOMAIN
+export SUBDOMAIN_TOKEN
+
+export ENVIRONMENT_NAME
+
+export REGION
+export STORAGE_LOCATION
+export AVAILABILITY_ZONE
+
+export DNS_ZONE
+export DNS_TTL
+
+export SERVICE_ACCOUNT_NAME
+export SERVICE_ACCOUNT
+
+export KEYDIR
+export WORKDIR
+export KEYFILE
+export MANIFEST_DIR
