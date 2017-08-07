217178b64611f5c92ca3916a5885741bcf5bce44
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-07 19:01:43 -0400

    Remove policy binding so delete succeeds (plus minor other things).

--- teardown.sh
+++ teardown.sh
@@ -8,9 +8,11 @@ bbl down --no-confirm
 
 echo "Deleting service accounts..."
 KEYID=`jq --raw-output '.private_key_id' "${KEYFILE}" `
+gcloud projects remove-iam-policy-binding "${PROJECT}" --member "serviceAccount:${SERVICE_ACCOUNT}" --role "roles/editor" --no-user-output-enabled
 gcloud iam service-accounts --project "${PROJECT}" keys delete "${KEYID}" --iam-account "${SERVICE_ACCOUNT}" --no-user-output-enabled
-gcloud iam service-accounts --project "${PROJECT}" delete "${SERVICE_ACCOUNT}"
+gcloud iam service-accounts --project "${PROJECT}" delete "${SERVICE_ACCOUNT}" --no-user-output-enabled
 
+chmod 400 ${KEYDIR}/id_jumpbox_${SUBDOMAIN_TOKEN}.pem
 rm -rf ${KEYDIR}/*
 rm -rf ${WORKDIR}/*
 rm bbl-state.json
