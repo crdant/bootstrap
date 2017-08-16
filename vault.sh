6b39ff626c361cede4943a22c6e4eeaec1a492d7
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-15 22:41:50 -0700

    Added vault initialization

--- vault.sh
+++ vault.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 STEMCELL_VERSION=3431.10
 VAULT_VERSION=3.3.4
+VAULT_STATIC_IP=10.0.31.195
 
 BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/env.sh"
@@ -16,10 +17,31 @@ releases () {
   bosh -e ${ENVIRONMENT_NAME} upload-release ${vault_release}
 }
 
+prepare_manifest () {
+  manifest=${WORKDIR}/vault.yml
+  spruce merge ${MANIFEST_DIR}/vault.yml > $manifest
+  VAULT_STATIC_IP=`spruce json $manifest | jq -r '.instance_groups[].networks[].static_ips'`
+}
+
 deploy () {
-  bosh -e ${ENVIRONMENT_NAME} -d vault deploy ${MANIFEST_DIR}/vault.yml
+  bosh -n -e ${ENVIRONMENT_NAME} -d vault deploy ${manifest}
+  ssh -fnNT -L 8200:${VAULT_STATIC_IP}:8200 jumpbox@${JUMPBOX} -i $BOSH_GW_PRIVATE_KEY
+  VAULT_ADDR=http://localhost:8200
+}
+
+init () {
+  # initialize the vault using the API directly to parse the JSON
+  initialization=`cat ${ETCDIR}/vault_init.json`
+  curl -q -X PUT "${VAULT_ADDR}/v1/sys/init" -H "Accept: application/json" -H "Content-Type: application/json" -d "${initialization}" | jq '.' >${KEYDIR}/vault_secrets.json
+
+  # unseal the vault
+  vault unseal `jq -r '.keys_base64[0]' ${KEYDIR}/vault_secrets.json`
+  vault unseal `jq -r '.keys_base64[1]' ${KEYDIR}/vault_secrets.json`
+  vault unseal `jq -r '.keys_base64[2]' ${KEYDIR}/vault_secrets.json`
 }
 
 stemcell
 releases
+prepare_manifest
 deploy
+init
