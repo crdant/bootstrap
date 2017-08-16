42d0f0f7a668b69e8a7e7e98e24cddab84d53151
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-16 07:48:04 -0700

    Added command-line options to run pieces of the script

--- configure.sh
+++ configure.sh
@@ -17,6 +17,29 @@ tokens () {
   vault token-create --policy concourse > "${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.token"
 }
 
+
+if [ $# -gt 0 ]; then
+  while [ $# -gt 0 ]; do
+    case $1 in
+      auth )
+        auth
+        ;;
+      policies )
+        policies
+        ;;
+      tokens )
+        tokens
+        ;;
+      * )
+        echo "Unrecognized option: $1" 1>&2
+        exit 1
+        ;;
+    esac
+    shift
+  done
+  exit
+fi
+
 auth
 policies
 tokens
--- vault.sh
+++ vault.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-STEMCELL_VERSION=3431.12
+STEMCELL_VERSION=3431.13
+STEMCELL_CHECKSUM=8ae6d01f01f627e70e50f18177927652a99a4585
 VAULT_VERSION=0.6.2
 VAULT_CHECKSUM=36fd3294f756372ff9fbbd6dfac11fe6030d02f9
 VAULT_STATIC_IP=10.0.31.195
@@ -12,7 +13,7 @@ stemcell () {
 }
 
 releases () {
-  bosh -e ${ENVIRONMENT_NAME} upload release https://bosh.io/d/github.com/cloudfoundry-community/vault-boshrelease?v=${VAULT_VERSION} --sha1 ${VAULT_CHECKSUM}
+  bosh -e ${ENVIRONMENT_NAME} upload-release https://bosh.io/d/github.com/cloudfoundry-community/vault-boshrelease?v=${VAULT_VERSION} --sha1 ${VAULT_CHECKSUM}
 }
 
 prepare_manifest () {
@@ -23,6 +24,9 @@ prepare_manifest () {
 
 deploy () {
   bosh -n -e ${ENVIRONMENT_NAME} -d vault deploy ${manifest}
+}
+
+tunnel () {
   ssh -fnNT -L 8200:${VAULT_STATIC_IP}:8200 jumpbox@${JUMPBOX} -i $BOSH_GW_PRIVATE_KEY
   VAULT_ADDR=http://localhost:8200
 }
@@ -38,8 +42,40 @@ init () {
   vault unseal `jq -r '.keys_base64[2]' ${KEYDIR}/vault_secrets.json`
 }
 
+if [ $# -gt 0 ]; then
+  while [ $# -gt 0 ]; do
+    case $1 in
+      stemcell )
+        stemcell
+        ;;
+      release )
+        release
+        ;;
+      manifest )
+          prepare_manifest
+        ;;
+      deploy )
+        deploy
+        ;;
+      tunnel )
+        tunnel
+        ;;
+      init )
+        init
+        ;;
+      * )
+        echo "Unrecognized option: $1" 1>&2
+        exit 1
+        ;;
+    esac
+    shift
+  done
+  exit
+fi
+
 stemcell
 releases
 prepare_manifest
 deploy
+tunnel
 init
