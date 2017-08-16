56bd64d2d92277e63b1358df9e4eb258db15bdf2
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-16 07:53:20 -0700

    Added command-line options to run pieces of the script

--- prepare.sh
+++ prepare.sh
@@ -3,24 +3,61 @@ BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/env.sh"
 set -e
 
-echo "Configuring service accounts..."
-gcloud iam service-accounts --project "${PROJECT}" create "${SERVICE_ACCOUNT_NAME}" --display-name "BOSH Boot Loader (bbl)" --no-user-output-enabled
-gcloud iam service-accounts --project "${PROJECT}" keys create "${KEYFILE}"  --iam-account "${SERVICE_ACCOUNT}" --no-user-output-enabled
+serivce_accounts() {
+  echo "Configuring service accounts..."
+  gcloud iam service-accounts --project "${PROJECT}" create "${SERVICE_ACCOUNT_NAME}" --display-name "BOSH Boot Loader (bbl)" --no-user-output-enabled
+  gcloud iam service-accounts --project "${PROJECT}" keys create "${KEYFILE}"  --iam-account "${SERVICE_ACCOUNT}" --no-user-output-enabled
+  gcloud projects add-iam-policy-binding "${PROJECT}" --member "serviceAccount:${SERVICE_ACCOUNT}" --role "roles/editor" --no-user-output-enabled
+}
 
-gcloud projects add-iam-policy-binding "${PROJECT}" --member "serviceAccount:${SERVICE_ACCOUNT}" --role "roles/editor" --no-user-output-enabled
+director() {
+  echo "Creating the BOSH director..."
+  set +e
+  bbl up --iaas gcp --gcp-service-account-key "${KEYFILE}" --gcp-project-id "${PROJECT}" --gcp-zone "${AVAILABILITY_ZONE}" --gcp-region "${REGION}" --jumpbox
+  set -e
+}
 
-echo "Creating the BOSH director..."
-set +e
-bbl up --iaas gcp --gcp-service-account-key "${KEYFILE}" --gcp-project-id "${PROJECT}" --gcp-zone "${AVAILABILITY_ZONE}" --gcp-region "${REGION}" --jumpbox
-set -e
+client() {
+  echo "Configuring BOSH client for the new director..."
+  eval "$(bbl print-env)"
+  bosh_client=`bbl director-username`
+  bosh_client_secret=`bbl director-password`
+  bosh_ca_cert=`bbl director-ca-cert`
+  bosh_director_address=`bbl director-address`
+  bosh alias-env --environment="${bosh_director_address}" --ca-cert="${bosh_ca_cert}" "${ENVIRONMENT_NAME}"
+}
 
-echo "Configuring BOSH client for the new director..."
-eval "$(bbl print-env)"
-bosh_client=`bbl director-username`
-bosh_client_secret=`bbl director-password`
-bosh_ca_cert=`bbl director-ca-cert`
-bosh_director_address=`bbl director-address`
-bosh alias-env --environment="${bosh_director_address}" --ca-cert="${bosh_ca_cert}" "${ENVIRONMENT_NAME}"
+login() {
+  echo "Logging into the new director..."
+  bosh log-in -e "${ENVIRONMENT_NAME}" --client="${bosh_client}" --client-secret="${bosh_client_secret}"
+}
 
-echo "Logging into the new director..."
-bosh log-in -e "${ENVIRONMENT_NAME}" --client="${bosh_client}" --client-secret="${bosh_client_secret}"
+if [ $# -gt 0 ]; then
+  while [ $# -gt 0 ]; do
+    case $1 in
+      accounts )
+        service_accounts
+        ;;
+      director )
+        director
+        ;;
+      client )
+        client
+        ;;
+      login )
+          login
+        ;;
+      * )
+        echo "Unrecognized option: $1" 1>&2
+        exit 1
+        ;;
+    esac
+    shift
+    exit
+  done
+fi
+
+service-accounts
+director
+client
+login
--- teardown.sh
+++ teardown.sh
@@ -3,16 +3,48 @@
 BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/env.sh"
 
-echo "Destroying the BOSH director..."
-bbl down --no-confirm
+director() {
+  echo "Destroying the BOSH director..."
+  bbl down --no-confirm
+}
 
-echo "Deleting service accounts..."
-KEYID=`jq --raw-output '.private_key_id' "${KEYFILE}" `
-gcloud projects remove-iam-policy-binding "${PROJECT}" --member "serviceAccount:${SERVICE_ACCOUNT}" --role "roles/editor" --no-user-output-enabled
-gcloud iam service-accounts --project "${PROJECT}" keys delete "${KEYID}" --iam-account "${SERVICE_ACCOUNT}" --no-user-output-enabled
-gcloud iam service-accounts --project "${PROJECT}" delete "${SERVICE_ACCOUNT}" --no-user-output-enabled
+service_accounts () {
+  echo "Deleting service accounts..."
+  KEYID=`jq --raw-output '.private_key_id' "${KEYFILE}" `
+  gcloud projects remove-iam-policy-binding "${PROJECT}" --member "serviceAccount:${SERVICE_ACCOUNT}" --role "roles/editor" --no-user-output-enabled
+  gcloud iam service-accounts --project "${PROJECT}" keys delete "${KEYID}" --iam-account "${SERVICE_ACCOUNT}" --no-user-output-enabled
+  gcloud iam service-accounts --project "${PROJECT}" delete "${SERVICE_ACCOUNT}" --no-user-output-enabled
+}
 
-chmod 400 ${KEYDIR}/id_jumpbox_${SUBDOMAIN_TOKEN}.pem
-rm -rf ${KEYDIR}/*
-rm -rf ${WORKDIR}/*
-rm bbl-state.json
+cleanup() {
+  chmod 400 ${KEYDIR}/id_jumpbox_${SUBDOMAIN_TOKEN}.pem
+  rm -rf ${KEYDIR}/*
+  rm -rf ${WORKDIR}/*
+  rm bbl-state.json
+}
+
+if [ $# -gt 0 ]; then
+  while [ $# -gt 0 ]; do
+    case $1 in
+      director )
+        director
+        ;;
+      accounts )
+        service_accounts
+        ;;
+      cleanup )
+          login
+        ;;
+      * )
+        echo "Unrecognized option: $1" 1>&2
+        exit 1
+        ;;
+    esac
+    shift
+    exit
+  done
+fi
+
+director
+service_accounts
+cleanups
