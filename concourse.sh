1c7c6daaa4680c9b35bc8d6876c3139e4775ab88
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-21 19:30:16 -0400

    Updated for new environment alias and vault integration

--- concourse.sh
+++ concourse.sh
@@ -6,17 +6,15 @@ CONCOURSE_CHECKSUM=e262b0fb209df6134ea15917e2b9b8bfb8d0d0d1
 GARDEN_RUNC_VERSION=1.6.0
 GARDEN_RUNC_CHECKSUM=58fbc64aff303e6d76899441241dd5dacef50cb7
 
-
 BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/env.sh"
+
+export ATC_KEY_FILE="${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.key"
+export ATC_CERT_FILE="${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.crt"
 
 ssl_certificates () {
   LB_KEY_FILE="${KEYDIR}/web-${SUBDOMAIN_TOKEN}.key"
   LB_CERT_FILE="${KEYDIR}/web-${SUBDOMAIN_TOKEN}.crt"
-
-  export ATC_KEY_FILE="${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.key"
-  export ATC_CERT_FILE="${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.crt"
-  export ATC_VAULT_TOKEN=`cat keys/atc-gcp-crdant-io.token | grep "token " | awk '{ print $2; }'`
 
   echo "Creating SSL certificate for load balancers..."
 
@@ -48,36 +46,42 @@ ssl_certificates () {
 }
 
 stemcell () {
-  bosh -e ${ENVIRONMENT_NAME} upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-trusty-go_agent?v=${STEMCELL_VERSION} --sha1 ${STEMCELL_CHECKSUM}
+  bosh -e "${env_id}" upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-trusty-go_agent?v=${STEMCELL_VERSION} --sha1 ${STEMCELL_CHECKSUM}
 }
 
 releases () {
   concourse_file=${WORKDIR}/concourse-${CONCOURSE_VERSION}.tgz
   garden_runc_file=${WORKDIR}/garden-runc-${GARDEN_RUNC_VERSION}.tgz
 
-  bosh -e ${ENVIRONMENT_NAME} upload-release https://bosh.io/d/github.com/concourse/concourse?v=${CONCOURSE_VERSION} --sha1 ${CONCOURSE_CHECKSUM}
-  bosh -e ${ENVIRONMENT_NAME} upload-release https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=${GARDEN_RUNC_VERSION} --sha1 ${GARDEN_RUNC_CHECKSUM}
+  bosh -e "${env_id}" upload-release https://bosh.io/d/github.com/concourse/concourse?v=${CONCOURSE_VERSION} --sha1 ${CONCOURSE_CHECKSUM}
+  bosh -e "${env_id}" upload-release https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=${GARDEN_RUNC_VERSION} --sha1 ${GARDEN_RUNC_CHECKSUM}
 }
 
 prepare_manifest () {
-  manifest=${WORKDIR}/concourse.yml
+  local manifest=${WORKDIR}/concourse.yml
+  export ATC_VAULT_TOKEN=`cat keys/atc-gcp-crdant-io.token | grep "token " | awk '{ print $2; }'`
+  export VAULT_CERT_FILE=${KEYDIR}/vault-${env_id}.crt
+
   spruce merge --prune tls ${MANIFEST_DIR}/concourse.yml > $manifest
 }
 
 deploy () {
+  local manifest=${WORKDIR}/concourse.yml
   bbl create-lbs --type concourse
-  bosh -n -e ${ENVIRONMENT_NAME} -d concourse deploy ${manifest}
+  bosh -n -e "${env_id}" -d concourse deploy "${manifest}"
 }
 
-init () {
-  vault
+teardown () {
+  bosh -n -e "${env_id}" -d concourse delete-deployment
 }
-
 
 if [ $# -gt 0 ]; then
   while [ $# -gt 0 ]; do
     case $1 in
       certificates )
+        ssl_certificates
+        ;;
+      security )
         ssl_certificates
         ;;
       stemcell )
@@ -93,7 +97,9 @@ if [ $# -gt 0 ]; then
         deploy
         ;;
       init )
-        init
+        ;;
+      teardown )
+        teardown
         ;;
       * )
         echo "Unrecognized option: $1" 1>&2
