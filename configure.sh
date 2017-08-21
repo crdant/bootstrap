d2f97b8e1cb4706590f28f33d85bd04f5396f25b
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-21 19:30:44 -0400

    Made vault connection explicit and TLS protected

--- configure.sh
+++ configure.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/env.sh"
+. ${WORKDIR}/bbl-env.sh
+
+env_id=`bbl env-id`
+
+VAULT_PORT=8200
+VAULT_ADDR=https://localhost:${VAULT_PORT}
+VAULT_CERT_FILE=${KEYDIR}/vault-${env_id}.crt
+
 set -e
 
 auth () {
-  vault auth `jq -r .root_token ${KEYDIR}/vault_secrets.json`
+  vault auth  --address ${VAULT_ADDR} --ca-cert=${VAULT_CERT_FILE} `jq -r .root_token ${KEYDIR}/vault_secrets.json`
 }
 
 policies () {
-  vault policy-write conrad ${ETCDIR}/conrad.hcl
-  vault policy-write concourse ${ETCDIR}/concourse.hcl
+  vault policy-write --address ${VAULT_ADDR} --ca-cert=${VAULT_CERT_FILE} conrad ${ETCDIR}/conrad.hcl
+  vault policy-write --address ${VAULT_ADDR} --ca-cert=${VAULT_CERT_FILE} concourse ${ETCDIR}/concourse.hcl
 }
 
 tokens () {
-  vault token-create --policy conrad > "${KEYDIR}/conrad-${SUBDOMAIN_TOKEN}.token"
-  vault token-create --policy concourse > "${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.token"
+  vault token-create --address ${VAULT_ADDR} --ca-cert=${VAULT_CERT_FILE} --policy conrad > "${KEYDIR}/conrad-${SUBDOMAIN_TOKEN}.token"
+  vault token-create --address ${VAULT_ADDR} --ca-cert=${VAULT_CERT_FILE} --policy concourse > "${KEYDIR}/atc-${SUBDOMAIN_TOKEN}.token"
 }
 
 
