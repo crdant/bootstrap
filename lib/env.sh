d96b3ebdad5746537b7a4d92d1c8af50d8464ae7
Chuck D'Antonio <c_dantonio@acm.org>
2017-09-06 15:11:44 -0400

    Moved IaaS variables ahead of BBL variables because of 4.4.x changes

--- lib/env.sh
+++ lib/env.sh
@@ -11,18 +11,6 @@ availability_zone_1="${region}-b"
 availability_zone_2="${region}-c"
 availability_zone_3="${region}-a"
 
-
-if [ -f "${BASEDIR}/bbl-state.json" ] ; then
-  jumpbox=`bbl jumpbox-address | cut -d':' -f1 `
-  env_id=`bbl env-id --gcp-service-account-key "${key_file}" --gcp-project-id "${project}"`
-  short_id=`bbl env-id --gcp-service-account-key "${key_file}" --gcp-project-id "${project}" | sed s/bbl-env-// | cut -dt -f1`
-else
-  env_id=`echo ${subdomain} | tr . -`
-fi
-
-dns_zone="${env_id}-dns"
-dns_ttl=60
-
 # TO DO: fixed up to env_id next time I tear down
 service_account_name=`echo ${subdomain} | tr . -`
 service_account="${service_account_name}@${project}.iam.gserviceaccount.com"
@@ -32,6 +20,17 @@ key_file="${key_dir}/${project}-${service_account_name}.json"
 workdir="${BASEDIR}/work"
 etc_dir="${BASEDIR}/etc"
 manifest_dir="${BASEDIR}/manifests"
+
+if [ -f "${BASEDIR}/bbl-state.json" ] ; then
+  jumpbox=`bbl jumpbox-address --gcp-service-account-key "${key_file}" --gcp-project-id "${project}" | cut -d':' -f1 `
+  env_id=`bbl env-id --gcp-service-account-key "${key_file}" --gcp-project-id "${project}"`
+  short_id=`bbl env-id --gcp-service-account-key "${key_file}" --gcp-project-id "${project}" | sed s/bbl-env-// | cut -dt -f1`
+else
+  env_id=`echo ${subdomain} | tr . -`
+fi
+
+dns_zone="${env_id}-dns"
+dns_ttl=60
 
 if [ -f "${workdir}/bbl-env.sh" ] ; then
   . ${workdir}/bbl-env.sh
