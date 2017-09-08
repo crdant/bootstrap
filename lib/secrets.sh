be1d7a5659ed7df8248d22c382ad354183c7956c
Chuck D'Antonio <c_dantonio@acm.org>
2017-09-08 10:19:17 -0400

    Added a function to get a bootstrap secret

--- lib/secrets.sh
+++ lib/secrets.sh
@@ -1,3 +1,9 @@
 safe_auth_bootstrap () {
   jq --raw-output '.auth.client_token' ${key_dir}/bootstrap-${env_id}-token.json | safe auth token
 }
+
+bootstrap_secret () {
+  local component=${1}
+  local secret=${2}
+  safe get secret/bootstrap/${component}/${secret}:value
+}
