af23459ccd9077de869608b27ac24a225c4ae56d
Chuck D'Antonio <c_dantonio@acm.org>
2017-12-27 17:18:38 -0500

    Functions to interact with bosh credhub

--- lib/secrets.sh
+++ lib/secrets.sh
@@ -7,3 +7,12 @@ bootstrap_secret () {
   local secret=${2}
   safe get secret/bootstrap/${component}/${secret}:value
 }
+
+credhub_auth () {
+  https_proxy=$BOSH_ALL_PROXY credhub login -s https://10.0.0.6:8844 -u credhub-cli -p $(bosh int --path /credhub_cli_password ${state_dir}/vars/director-vars-store.yml ) \
+    --ca-cert <(cat <(bosh int --path /default_ca/ca ${state_dir}/vars/director-vars-store.yml) <(bosh int --path /credhub_ca/ca ${state_dir}/vars/director-vars-store.yml))
+}
+
+bosh_secret () {
+  https_proxy=$BOSH_ALL_PROXY credhub $@
+}
