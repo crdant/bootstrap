05a3d2eeb45bf4431c761ce36277f482c71428fd
Chuck D'Antonio <c_dantonio@acm.org>
2018-04-13 13:48:30 -0400

    Complete account creation and authorization

--- lib/aws/prepare.sh
+++ lib/aws/prepare.sh
@@ -1,18 +1,28 @@
+iam_policy() {
+  aws_iam_policy="$(curl -qLs "https://raw.githubusercontent.com/cloudfoundry-incubator/bosh-aws-cpi-release/master/docs/iam-policy.json" | jq '{ "Version": .Version, "Statement": [ .Statement[] | select ( .Sid != "RequiredIfUsingCustomKMSKeys" ) ] }')"
+  aws iam create-policy --policy-name ${env_id}-policy --policy-document "${aws_iam_policy}" > ${workdir}/aws_iam_policy.json
+  policy_arn="$(cat ${workdir}/aws_iam_policy.json | jq --raw-output '.Policy.Arn' )"
+}
+
 iam_accounts() {
   echo "Configuring IAM account..."
-  aws iam create-user --user-name ${iam_account_name} > ${key_file}
+  iam_policy
+  aws iam create-user --user-name ${iam_account_name} 2> /dev/null 1> /dev/null
+  aws iam create-access-key --user-name ${iam_account_name} > ${key_file}
+  aws iam attach-user-policy --user-name ${iam_account_name} --policy-arn ${policy_arn}
   access_key_id=$(cat ${key_file} | jq --raw-output '.AccessKey.AccessKeyId')
   secret_access_key=$(cat ${key_file} | jq --raw-output '.AccessKey.SecretAccessKey')
+  BBL_AWS_ACCESS_KEY_ID=${access_key_id}
+  BBL_AWS_SECRET_ACCESS_KEY=${secret_access_key}
 }
 
 firewall() {
   # add missing firewall rule to allow jumpbox to tunnel to all BOSH managed vms -- needed for BOSH ssh among other things
-  env_id=`bbl env-id --state-dir ${state_dir}`
-  gcloud --project "${project}" compute firewall-rules update ${env_id}-bosh-open --target-tags=${env_id}-bosh-director,${env_id}-internal
+  echo "do I still need this"
 }
 
 dns () {
-  gcloud dns managed-zones --project ${project} create ${dns_zone} --dns-name "${subdomain}." --description "Zone for ${subdomain}"
+  aws route53 create-hosted-zone ${subdomain} --caller-reference ${env_id}-$(date +"%s") --hosted-zone-config Comment="Zone for ${subdomain} - bbl env ${env_id}"
 
   # TO DO: put this in here like in https://github.com/crdant/pcf-on-gcp
   # update_root_dns
--- lib/aws/teardown.sh
+++ lib/aws/teardown.sh
@@ -1,4 +1,12 @@
+iam_policy() {
+  policy_arn="$(cat ${workdir}/aws_iam_policy.json | jq --raw-output '.Policy.Arn' )"
+  aws iam delete-policy --policy-arn ${policy_arn}
+}
+
 iam_accounts() {
   echo "Deleting IAM account..."
+  aws iam detach-user-policy --user-name ${iam_account_name} --policy-arn "$(cat ${workdir}/aws_iam_policy.json | jq --raw-output '.Policy.Arn' )"
+  aws iam delete-access-key --user-name ${iam_account_name} --access-key-id $(cat ${key_file} | jq --raw-output '.AccessKey.AccessKeyId')
   aws iam delete-user --user-name ${iam_account_name}
+  iam_policy
 }
