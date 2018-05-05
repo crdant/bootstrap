8bd7d1cf3a869ee84f4b0907a5c89adc5ccd9d70
Chuck D'Antonio <c_dantonio@acm.org>
2018-05-05 15:33:11 -0400

    Deploy concourse on AWS using plan patch environment

--- lib/aws/concourse.sh
+++
@@ -1,5 +0,0 @@
-dns() {
-  echo "Setting up DNS..."
-  local hostname="$(bbl lbs | grep "^Concourse LB:" | grep -o '\[.*\]' | tr -d [ | tr -d ])"
-  add_dns_alias ${dns_zone_id} ${concourse_host} ${hostname} "Bootstrap concourse for ${env_id}"
-}
---
+++ patches/aws/concourse/terraform/concourse_dns.tf
@@ -0,0 +1,12 @@
+variable "concourse_host" {
+  type = "string"
+}
+
+resource "aws_route53_record" "concourse_dns" {
+  zone_id = "${aws_route53_zone.bootstrap_dns_zone.id}"
+  name    = "${var.concourse_host}"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = ["${aws_lb.concourse_lb.dns_name}"]
+}
