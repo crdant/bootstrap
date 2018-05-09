55851b79c3d4cbfba452284fe670ab26feaa29bd
Chuck D'Antonio <c_dantonio@acm.org>
2018-05-09 17:36:21 -0400

    Code complete AWS implementation with plan patches

--- lib/aws/pcf.sh
+++ lib/aws/pcf.sh
@@ -1,16 +1,29 @@
 install_iaas_params () {
+  safe_auth
+  safe set ${team_secret_root}/aws_access_key_id value="${pcf_secret_access_key_id}"
+  safe set ${team_secret_root}/aws_secret_access_key value="${pcf_secret_access_key}"
+
+  safe set ${deploy_secret_root}/PEM value="$(cat $pcf_private_key_file)"
+  safe set ${deploy_secret_root}/mysql_backups_s3_access_key_id value=${pcf_secret_access_key_id}
+  safe set ${deploy_secret_root}/mysql_backups_s3_secret_access_key value=${pcf_secret_access_key}
+  safe set ${deploy_secret_root}/director_certificates value="$(cat ${ca_cert_file})"
+
   cat <<PARAMS >> ${pcf_install_parameter_file}
-aws_access_key_id: ${pcf_secret_access_key_id}
-aws_secret_access_key: ${pcf_secret_access_key}
 aws_az1: ${availability_zone_1}
 aws_az2: ${availability_zone_2}
 aws_az3: ${availability_zone_3}
 aws_cert_arn: ${pcf_wildcard_cert_arn}
-aws_key_name: ${default_key_name}
+aws_key_name: ${env_id}-pcf-key
 aws_region: ${region}
-ROUTE_53_ZONE_ID: ${bootstrap_dns_zone_id}
+mysql_backups: s3
+mysql_backups_s3_bucket_name: ${mysql_backup_bucket}
+mysql_backups_s3_bucket_path: mysql_backups
+mysql_backups_s3_cron_schedule: "22 4 * * 0"
+mysql_backups_s3_endpoint_url: https://s3.${region}.amazonaws.com
+ROUTE_53_ZONE_ID: ${pcf_dns_zone_id}
+# For terraform state file (http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region)
+S3_ENDPOINT: https://s3.${region}.amazonaws.com
 S3_OUTPUT_BUCKET: ${pipeline_statefile_bucket}
-PEM: ${private_key}
 PARAMS
 }
 
