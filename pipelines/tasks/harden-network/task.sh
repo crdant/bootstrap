d7b0b436efca2d4ef11564d49ac315bc9a5ab656
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-08 15:37:45 -0400

    Fixup flow of pipelines files

--- pipelines/tasks/harden-network/task.sh
+++ pipelines/tasks/harden-network/task.sh
@@ -4,6 +4,11 @@ set -eu
 
 var_file=pcf-pipelines/install-pcf/${iaas}/terraform/harden.auto.tfvars
 
+copy_templates () {
+  cp -R pcf-pipelines-base/* pcf-pipelines
+  cp bootstrap/pipelines/terraform/${iaas}/*.tf pcf-pipelines/install-pcf/${iaas}/terraform
+}
+
 get_ips() {
   cloudfront_ips=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq --arg region "${region}" '[ .prefixes[] | select(.service=="CLOUDFRONT" and .region==$region) | .ip_prefix  ]')
   ec2_ips=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq --arg region "${region}" '[ .prefixes[] | select(.service=="EC2" and .region==$region) |  .ip_prefix  ]')
@@ -22,5 +27,3 @@ TFVARS
 
 get_ips
 write_var_file
-
-cp bootstrap/pipelines/terraform/${iaas}/*.tf pcf-pipelines/install-pcf/${iaas}/terraform
