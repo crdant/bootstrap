59c8c408e6f3b2478b5225ffbea0ee05b3eb89de
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-08 14:51:29 -0400

    Match case on parameter/variable

--- pipelines/tasks/harden-network/task.sh
+++ pipelines/tasks/harden-network/task.sh
@@ -5,8 +5,8 @@ set -eu
 var_file=pcf-pipelines/install-pcf/${iaas}/terraform/harden.auto.tfvars
 
 get_ips() {
-  cloudfront_ips=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq --arg region "${REGION}" '[ .prefixes[] | select(.service=="CLOUDFRONT" and .region==$region) | .ip_prefix  ]')
-  ec2_ips=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq --arg region "${REGION}" '[ .prefixes[] | select(.service=="EC2" and .region==$region) |  .ip_prefix  ]')
+  cloudfront_ips=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq --arg region "${region}" '[ .prefixes[] | select(.service=="CLOUDFRONT" and .region==$region) | .ip_prefix  ]')
+  ec2_ips=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq --arg region "${region}" '[ .prefixes[] | select(.service=="EC2" and .region==$region) |  .ip_prefix  ]')
   github_ips=$(curl -s https://api.github.com/meta | jq '.git')
 }
 
