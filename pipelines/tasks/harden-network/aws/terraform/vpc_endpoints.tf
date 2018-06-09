f7c70b68beff30f3842af8cb75d2e5b59d1bac82
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-08 20:19:17 -0400

    Define the policy inline to see if it resolves things

--- pipelines/tasks/harden-network/aws/terraform/vpc_endpoints.tf
+++ pipelines/tasks/harden-network/aws/terraform/vpc_endpoints.tf
@@ -9,46 +9,38 @@ resource "aws_vpc_endpoint" "ec2" {
   ]
 }
 
-resource "aws_iam_policy" "blobstore_access" {
-  name = "blobstore_endpoint_access_policy"
-  path = "/"
-
-  policy = <<POLICY
-{
-  "Statement": [
-    {
-      "Sid": "Access-to-blobstore-buckets-only",
-      "Principal": "*",
-      "Action": [
-        "s3:GetObject",
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": [ "arn:aws:s3:::${aws_s3_bucket.bosh.bucket},
-                    "arn:aws:s3:::${aws_s3_bucket.bosh.bucket}/*",
-                    "arn:aws:s3:::${aws_s3_bucket.buildpacks.bucket},
-                    "arn:aws:s3:::${aws_s3_bucket.buildpacks.bucket}/*",
-                    "arn:aws:s3:::${aws_s3_bucket.droplets.bucket},
-                    "arn:aws:s3:::${aws_s3_bucket.droplets.bucket}/*",
-                    "arn:aws:s3:::${aws_s3_bucket.packages.bucket},
-                    "arn:aws:s3:::${aws_s3_bucket.packages.bucket}/*",
-                    "arn:aws:s3:::${aws_s3_bucket.resources.bucket},
-                    "arn:aws:s3:::${aws_s3_bucket.resources.bucket}/*" ]
-    }
-  ]
-}
-POLICY
-}
-
 resource "aws_vpc_endpoint" "s3" {
   vpc_id             = "${aws_vpc.PcfVpc.id}"
   service_name       = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type  = "Gateway"
 
-  policy             = "${aws_iam_policy.blobstore_access.arn}"
-
   security_group_ids = [
     "${aws_security_group.cloud_controller.id}",
     "${aws_security_group.directorSG.id}"
   ]
+
+  policy = <<POLICY
+{
+"Statement": [
+  {
+    "Sid": "Access-to-blobstore-buckets-only",
+    "Principal": "*",
+    "Action": [
+      "s3:::*"
+    ],
+    "Effect": "Allow",
+    "Resource": [ "arn:aws:s3:::${aws_s3_bucket.bosh.bucket},
+                  "arn:aws:s3:::${aws_s3_bucket.bosh.bucket}/*",
+                  "arn:aws:s3:::${aws_s3_bucket.buildpacks.bucket},
+                  "arn:aws:s3:::${aws_s3_bucket.buildpacks.bucket}/*",
+                  "arn:aws:s3:::${aws_s3_bucket.droplets.bucket},
+                  "arn:aws:s3:::${aws_s3_bucket.droplets.bucket}/*",
+                  "arn:aws:s3:::${aws_s3_bucket.packages.bucket},
+                  "arn:aws:s3:::${aws_s3_bucket.packages.bucket}/*",
+                  "arn:aws:s3:::${aws_s3_bucket.resources.bucket},
+                  "arn:aws:s3:::${aws_s3_bucket.resources.bucket}/*" ]
+  }
+]
+}
+POLICY
 }
