98a364271369781307f3e47d3d6cf22a307e4fcd
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-08 21:31:32 -0400

    Update TCP router ingress

--- pipelines/tasks/harden-network/aws/terraform/security_group.tf
+++ pipelines/tasks/harden-network/aws/terraform/security_group.tf
@@ -174,8 +174,8 @@ resource "aws_security_group_rule" "pcfSG_ingress_PcfSshElbSg_2222" {
 
 resource "aws_security_group_rule" "pcfSG_ingress_PcfTcpElbSg" {
   type            = "ingress"
-  from_port       = 0
-  to_port         = 0
+  from_port       = 1024
+  to_port         = 65535
   protocol        = "-1"
   source_security_group_id = "${aws_elb.PcfTcpElb.source_security_group_id}"
   security_group_id = "${aws_security_group.pcfSG.id}"
