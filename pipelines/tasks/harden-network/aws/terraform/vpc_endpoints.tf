23b2fa631956ad203cb3f884366dd85601657f37
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-19 10:30:48 -0400

    Polish

--- pipelines/tasks/harden-network/aws/terraform/vpc_endpoints.tf
+++ pipelines/tasks/harden-network/aws/terraform/vpc_endpoints.tf
@@ -18,6 +18,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id             = "${aws_vpc.PcfVpc.id}"
   service_name       = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type  = "Gateway"
+  
   route_table_ids    = [
       "${aws_route_table.PublicSubnetRouteTable.id}",
       "${aws_route_table.PrivateSubnetRouteTable_az1.id}",
