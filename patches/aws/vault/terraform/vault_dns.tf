7508a3b5be743843483bfce22819024dfa02c73e
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-26 19:07:02 -0400

    Adjust for lessons learned while implementing GCP templates

--- patches/aws/vault/terraform/vault_dns.tf
+++ patches/aws/vault/terraform/vault_dns.tf
@@ -2,11 +2,15 @@ variable "vault_host" {
   type = "string"
 }
 
+locals {
+  vault_fqdn = "${var.vault_host}.${var.subdomain}"
+}
+
 resource "aws_route53_record" "vault_dns" {
   zone_id = "${aws_route53_zone.bootstrap_dns_zone.id}"
-  name    = "${var.vault_host}"
+  name    = "${local.vault_fqdn}"
   type    = "CNAME"
-  ttl     = 300
+  ttl  = "${var.dns_ttl}"
 
   records = ["${aws_lb.vault_lb.dns_name}"]
 }
