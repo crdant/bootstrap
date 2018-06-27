9671a3979b5c6c9943dd028b9f38910bdd00dc24
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-27 17:37:42 -0400

    Make credentials explicit

--- patches/gcp/pcf/terraform/remote_state.tf
+++ patches/gcp/pcf/terraform/remote_state.tf
@@ -2,5 +2,6 @@ data "terraform_remote_state" "pcf_pipelines" {
   backend = "gcs"
   config {
     bucket  = "${local.short_env_id}-terraform-state"
+    credentials = "${var.key_dir}/${google_service_account.pcf_service_account.email}.json"
   }
 }
