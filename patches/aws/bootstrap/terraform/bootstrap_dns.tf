761749056b3037573d7ee059e2308b628d1c98c5
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-02 14:33:36 -0400

    Configure DNS delegation for the bootstrap domain

--- patches/aws/bootstrap/terraform/bootstrap_dns.tf
+++ patches/aws/bootstrap/terraform/bootstrap_dns.tf
@@ -2,12 +2,29 @@ variable "bootstrap_domain" {
   type = "string"
 }
 
+locals {
+  cloud_subdomain = "aws.${var.domain}"
+}
+
+data "aws_route53_zone" "cloud" {
+  name = "${local.cloud_subdomain}"
+}
+
 resource "aws_route53_zone" "bootstrap_dns_zone" {
   name = "${var.bootstrap_domain}"
 
   tags {
     Name = "${var.env_id}-bootstrap-zone"
   }
+}
+
+resource "aws_route53_record" "bootstrap_delgation_dns" {
+  zone_id = "${data.aws_route53_zone.cloud.id}"
+  name    = "${var.bootstrap_domain}"
+  type    = "NS"
+  ttl     = 300
+
+  records = ["${aws_route53_zone.bootstrap_dns_zone.name_servers}"]
 }
 
 output "bootstrap_dns_zone_id" {
--- patches/aws/bootstrap/terraform/shared.tf
+++ patches/aws/bootstrap/terraform/shared.tf
@@ -1,3 +1,7 @@
+variable "domain" {
+  type = "string"
+}
+
 variable "email" {
   type = "string"
 }
