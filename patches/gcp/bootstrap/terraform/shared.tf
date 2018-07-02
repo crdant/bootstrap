099be859d220a4227559812f5814fb915717eac9
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-02 12:30:29 -0400

    Add common variables

--- patches/gcp/bootstrap/terraform/shared.tf
+++ patches/gcp/bootstrap/terraform/shared.tf
@@ -5,12 +5,3 @@ variable "email" {
 variable "key_dir" {
   type = "string"
 }
-
-locals {
-  env_components = "${split("-", var.env_id)}"
-  short_env_id = "${join("-", slice(local.env_components, 0, 3))}"
-}
-
-output "short_env_id" {
-  value = "${local.short_env_id}"
-}
