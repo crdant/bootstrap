8d71d8e263576e3bee23d1530677d3266ab09e88
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-02 15:40:56 -0400

    Fixup TTL variable reference

--- patches/aws/concourse/terraform/concourse_dns.tf
+++ patches/aws/concourse/terraform/concourse_dns.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "concourse_dns" {
   zone_id = "${aws_route53_zone.bootstrap_dns_zone.id}"
   name    = "${var.concourse_host}"
   type    = "CNAME"
-  ttl     = "${var.bootstrap_dns_ttl}"
+  ttl     = "${var.dns_ttl}"
 
   records = ["${aws_lb.concourse_lb.dns_name}"]
 }
